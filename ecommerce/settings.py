fae01024a4fbfc0a7d08fdcd84ca8eab9276d044
Ibrooo123 < @example.com>
2025-08-29 16:17:15 +0300

    تعديل ملف sittings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -15,8 +15,9 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
